4cdb2d0e337b7dd22121d15e24f9998d1aad72a2
Chris Ayoub <chriswayoub@gmail.com>
2022-01-08 11:15:35 -0700

    cache access node list and make sure file gets closed

--- src/beam/beam.go
+++ src/beam/beam.go
@@ -51,8 +51,13 @@ type AccessNodeInfo struct {
 	IsLegacy    bool
 }
 
+var accessNodesLoaded = false
+var accessNodes []AccessNodeInfo
+
 func GetAccessNodes() []AccessNodeInfo {
-	var accessNodes []AccessNodeInfo
+	if accessNodesLoaded {
+		return accessNodes
+	}
 
 	accessNodesFile := os.Getenv("ACCESS_NODES")
 
@@ -66,13 +71,21 @@ func GetAccessNodes() []AccessNodeInfo {
 		panic(err)
 	}
 
+	defer file.Close()
+
 	b, err := ioutil.ReadAll(file)
+
+	if err != nil {
+		panic(err)
+	}
 
 	err = json.Unmarshal(b, &accessNodes)
 
 	if err != nil {
 		panic(err)
 	}
+
+	accessNodesLoaded = true
 
 	return accessNodes
 }
